5bbc51b81c88bab64892572d8ffc6c46830e3aa3
bushrahurre959@gmail.com <marriamhurre@gmail.com>
2024-02-05 10:34:25 +0300

    adding players name and symbol

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
-}
-
-export default App
-{/* <div id="game-container">
+    <main>
+      <div id="game-container">
         <ol id="players">
           <li>
-            <span className="player-name">Player 1</span>{" "}
-            <span className="palyer-symbol">X</span>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </span>
           </li>
           <li>
-            <span className="player-name">Player 2</span>{" "}
-            <span className="palyer-symbol">O</span>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </span>
           </li>
-        </ol> */}
-      // </div>
+          game
+        </ol>
+        log
+      </div>
+    </main>
+  );
+}
+
+export default App;
