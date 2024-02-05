1e85feb6fc6f972f899e00118a557cd32013fe4e
bushrahurre959@gmail.com <marriamhurre@gmail.com>
2024-02-05 10:26:01 +0300

    first commits

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+function App() {
+  
+
+  return (
+    <h1>React Tic-Tac-Toe</h1>
+  )
+}
+
+export default App
+{/* <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>{" "}
+            <span className="palyer-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>{" "}
+            <span className="palyer-symbol">O</span>
+          </li>
+        </ol> */}
+      // </div>
